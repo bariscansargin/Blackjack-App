900d6b6c5998d2de8675d2cf033dafc3c3529371
Barışcan Sargın <bariscansargin4@gmail.com>
2023-06-01 10:26:50 +0300

    Created header.

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='h-24 flex justify-center items-center'>
+      <p className='text-white font-bold text-2xl italic'>BLACKJACK</p>
+    </header>
+  )
+}
+
+export default Header

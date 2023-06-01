8a4f131e882058957d634189821ab2613900e500
Barışcan Sargın <bariscansargin4@gmail.com>
2023-06-01 10:27:17 +0300

    Created footer.

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { DiGithubBadge } from "react-icons/di";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <footer className="h-12 flex justify-center items-center ">
+      <p className="text-white text-sm mr-4">Made by Barışcan Sargın</p>
+      <Link to={"https://github.com/bariscansargin"}>
+        <DiGithubBadge className="w-8 h-8 text-white" />
+      </Link>
+    </footer>
+  );
+};
+
+export default Footer;

780b98859355b6ba5034d2e04ba0f19462a80eff
Barışcan Sargın <bariscansargin4@gmail.com>
2023-06-02 17:21:16 +0300

    Created card component.

---
+++ src/components/PlayingCard/PlayingCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PlayingCard = ({value, type}) => {
+    return (
+    <div className="flex flex-col">
+      <div className="w-full"></div>
+      <div className=" flex justify-center items-center w-full"></div>
+      <div className="w-full"></div>
+    </div>
+  );
+};
+
+export default PlayingCard;

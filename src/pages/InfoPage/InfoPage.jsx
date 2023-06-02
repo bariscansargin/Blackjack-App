a7bd434d5c6e8d0af14f45c86e1035bbee217028
Barışcan Sargın <bariscansargin4@gmail.com>
2023-06-02 17:20:09 +0300

    Created Info Page.

--- src/pages/InfoPage/InfoPage.jsx
+++ src/pages/InfoPage/InfoPage.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import GameForm from "../../components/GameForm/GameForm";
 
 const InfoPage = () => {
-  return <main className="flex-grow">InfoPage</main>;
+  return (
+    <main className="flex-grow flex flex-col items-center mt-4 ">
+      <p className="text-white text-md mb-12  ">
+        Fill the form to get start!
+      </p>
+      <GameForm />
+    </main>
+  );
 };
 
 export default InfoPage;

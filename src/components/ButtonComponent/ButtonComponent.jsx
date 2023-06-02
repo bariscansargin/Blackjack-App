d06816e26936afa004be9f35e17c5aa1d3086db5
Barışcan Sargın <bariscansargin4@gmail.com>
2023-06-02 17:19:32 +0300

    Update Button Component.

--- src/components/ButtonComponent/ButtonComponent.jsx
+++ src/components/ButtonComponent/ButtonComponent.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import cn from "classnames";
 
 const ButtonComponent = ({ value, children, position, clickHandler }) => {
-  const buttonStyles = cn("border-none outline-none py-1 px-2 rounded-lg text-white " + position, {
+  const buttonStyles = cn("border-none outline-none py-1 px-2 rounded-lg text-white cursor-pointer " + position, {
     "bg-green-600 hover:bg-green-500 ": value === "start",
     "bg-red-600 hover:bg-red-500": value === "reset",
   });

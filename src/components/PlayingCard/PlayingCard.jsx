74f42c3cba6dfd508c43f7170e741c1b34a5de6f
Barışcan Sargın <bariscansargin4@gmail.com>
2023-06-05 13:05:24 +0300

    Created card component.

--- src/components/PlayingCard/PlayingCard.jsx
+++ src/components/PlayingCard/PlayingCard.jsx
@@ -1,11 +1,32 @@
 import React from "react";
+import {
+  getCardColor,
+  getCardSuitIcon,
+  getCardText,
+} from "../../utils/game-functions";
 
-const PlayingCard = ({value, type}) => {
-    return (
-    <div className="flex flex-col">
-      <div className="w-full"></div>
-      <div className=" flex justify-center items-center w-full"></div>
-      <div className="w-full"></div>
+const PlayingCard = ({ value, suit }) => {
+  const cardColor = getCardColor(suit);
+
+  const cardSuitIcon = () => {
+    return <span className={cardColor}>{getCardSuitIcon(suit)}</span>;
+  };
+
+  return (
+    <div className="flex flex-col border w-16 bg-white p-1 rounded-md">
+      <div className={"w-full flex justify-start " + cardColor}>
+        <p className={cardColor}>{getCardText(value)}</p>
+      </div>
+      {value && suit && (
+        <div className=" flex flex-col justify-center items-center w-full flex-grow h-12">
+          <p className="text-sm">{cardSuitIcon()}</p>
+          <p className="text-sm">{cardSuitIcon()}</p>
+          <p className="text-sm">{cardSuitIcon()}</p>
+        </div>
+      )}
+      <div className={"w-full flex justify-end " + cardColor}>
+        {getCardText(value)}
+      </div>
     </div>
   );
 };
--- src/utils/game-functions.js
+++ src/utils/game-functions.js
@@ -2,8 +2,10 @@ import {
   IMAGED_CARD_TEXT_MAPPING,
   IMAGED_CARD_VALUES_MAPPING,
   IMAGED_CARD_COLORS_MAPPING,
+  IMAGED_CARD_ICON_MAPPING,
 } from "../../lib/constants";
 
 export const getCardValue = (value) =>IMAGED_CARD_VALUES_MAPPING[value] || +value;
 export const getCardColor = (suit) => IMAGED_CARD_COLORS_MAPPING[suit];
 export const getCardText = (value) => IMAGED_CARD_TEXT_MAPPING[value] || value;
+export const getCardSuitIcon = (value) => IMAGED_CARD_ICON_MAPPING[value];

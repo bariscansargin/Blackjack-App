d256e9637325c35a42bec709a1c5c2f91ca6b1bd
Barışcan Sargın <bariscansargin4@gmail.com>
2023-06-06 19:59:47 +0300

    Added getDeckValue function.

--- src/utils/game-functions.js
+++ src/utils/game-functions.js
@@ -5,8 +5,20 @@ import {
   IMAGED_CARD_ICON_MAPPING,
 } from "../../lib/constants";
 
-export const getCardValue = (value) =>IMAGED_CARD_VALUES_MAPPING[value] || +value;
+export const getCardValue = (value) =>
+  IMAGED_CARD_VALUES_MAPPING[value] || +value;
 export const getCardColor = (suit) => IMAGED_CARD_COLORS_MAPPING[suit];
 export const getCardText = (value) => IMAGED_CARD_TEXT_MAPPING[value] || value;
 export const getCardSuitIcon = (value) => IMAGED_CARD_ICON_MAPPING[value];
 
+export const getDeckValue = (arr) => {
+  const deckValue = arr.reduce((deckValue, card) => {
+    if (card.value === "ACE") {
+      deckValue += 1;
+    } else {
+      deckValue += getCardValue(card.value);
+    }
+    return deckValue;
+  }, 0);
+  return deckValue;
+};

821a5bdf81b6ee9ce1ea88f47bd2aea38542f9e6
Barışcan Sargın <bariscansargin4@gmail.com>
2023-06-06 17:01:46 +0300

    Update PlayingCard.

--- src/components/PlayingCard/PlayingCard.jsx
+++ src/components/PlayingCard/PlayingCard.jsx
@@ -4,30 +4,42 @@ import {
   getCardSuitIcon,
   getCardText,
 } from "../../utils/game-functions";
+import { cardBack } from "../../utils/card-design";
 
-const PlayingCard = ({ value, suit }) => {
+const PlayingCard = ({ value, suit, hidden }) => {
   const cardColor = getCardColor(suit);
-
+  const starsArr = cardBack()
   const cardSuitIcon = () => {
     return <span className={cardColor}>{getCardSuitIcon(suit)}</span>;
   };
 
   return (
-    <div className="flex flex-col border w-16 bg-white p-1 rounded-md shadow-lg ml-2">
-      <div className={"w-full flex justify-start " + cardColor}>
-        <p className={cardColor}>{getCardText(value)}</p>
-      </div>
+    <>
+      {!hidden && (
+        <div className="flex flex-col border w-16 bg-white p-1 rounded-md shadow-lg ml-2">
+          <div className={"w-full flex justify-start " + cardColor}>
+            <p className={cardColor}>{hidden ? "?" : getCardText(value)}</p>
+          </div>
 
-      <div className=" flex flex-col justify-center items-center w-full flex-grow h-12">
-        <p className="text-sm">{cardSuitIcon()}</p>
-        <p className="text-sm">{cardSuitIcon()}</p>
-        <p className="text-sm">{cardSuitIcon()}</p>
-      </div>
+          <div className=" flex flex-col justify-center items-center w-full flex-grow h-12">
+            <p className="text-sm">{hidden ? "?" : cardSuitIcon()}</p>
+            <p className="text-sm">{hidden ? "?" : cardSuitIcon()}</p>
+            <p className="text-sm">{hidden ? "?" : cardSuitIcon()}</p>
+          </div>
 
-      <div className={"w-full flex justify-end " + cardColor}>
-        {getCardText(value)}
-      </div>
-    </div>
+          <div className={"w-full flex justify-end " + cardColor}>
+            {hidden ? "?" : getCardText(value)}
+          </div>
+        </div>
+      )}
+      {hidden && (
+        <div className="flex flex-wrap border w-16 bg-red-400 p-4 rounded-md shadow-lg ml-2 ">
+          {starsArr.map((star,idx) => {
+            return <span key={idx} className="text-white">{star}</span>;
+          })}
+        </div>
+      )}
+    </>
   );
 };
 
---
+++ src/utils/card-design.jsx
@@ -0,0 +1,8 @@
+export function cardBack() {
+  const starArr = [];
+  for (let index = 0; index < 12; index++) {
+    starArr.push("*");
+  }
+
+  return starArr;
+}

b2253a98a0fb975b4c71d08874db3a9b61a076f4
Barışcan Sargın <bariscansargin4@gmail.com>
2023-06-06 17:03:50 +0300

    Updated scoreboard component.

--- src/Scoreboard/Scoreboard.jsx
+++ src/Scoreboard/Scoreboard.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import classNames from "classnames";
 
 const Scoreboard = ({ deckValue, type }) => {
-  const scorebardClasses = classNames("text-white text-md italic mt-2", {
+  const scorebardClasses = classNames(" text-md italic mt-2", {
+    "text-white": deckValue < 21,
     "text-red-700": deckValue > 21,
+    "text-green-300": deckValue === 21,
   });
   return (
     <div className={scorebardClasses}>
       <p>
-        {type} Score = {deckValue} / 21
+        {type} Score = {deckValue}
+        {deckValue === 21 && <span>( BLACKJACK!)</span>}
       </p>
     </div>
   );

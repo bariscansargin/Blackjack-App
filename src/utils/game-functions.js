5769b08f8bcd4e2ee417fd2d80d7905aae477470
Barışcan Sargın <bariscansargin4@gmail.com>
2023-07-03 13:11:16 +0300

    Solved deckValue problem.

--- src/utils/game-functions.js
+++ src/utils/game-functions.js
@@ -5,14 +5,15 @@ import {
   IMAGED_CARD_ICON_MAPPING,
 } from "../../lib/constants";
 
-export const getCardValue = (value) => IMAGED_CARD_VALUES_MAPPING[value] || +value;
+export const getCardValue = (value) =>
+  IMAGED_CARD_VALUES_MAPPING[value] || +value;
 export const getCardColor = (suit) => IMAGED_CARD_COLORS_MAPPING[suit];
 export const getCardText = (value) => IMAGED_CARD_TEXT_MAPPING[value] || value;
 export const getCardSuitIcon = (value) => IMAGED_CARD_ICON_MAPPING[value];
 
 export const getDeckValue = (arr) => {
   const deckValue = arr
-    .sort((a, b) => getCardValue(a) - getCardValue(b))
+    .sort((a, b) => getCardValue(a.value) - getCardValue(b.value))
     .reduce((deckValue, card) => {
       let cardValue = getCardValue(card.value);
       if (card.value === "ACE" && deckValue + cardValue > 21) {
@@ -24,4 +25,3 @@ export const getDeckValue = (arr) => {
 
   return deckValue;
 };
- 
